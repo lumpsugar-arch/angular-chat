875cdbb1628593fe675fae3176b1f85e6f15d503
Roman Afanasyev <afanasyev.r@yandex.ru>
2019-09-05 11:17:41 +0300

    Init angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: 'chat', component: ChatComponent },
+  { path: '',   redirectTo: '/chat', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
