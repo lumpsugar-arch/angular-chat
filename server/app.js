6bc63c61e5732360329b8f4f430057b6fe8ea759
Roman Afanasyev <afanasyev.r@yandex.ru>
2019-09-10 09:49:20 +0300

    Refactor server side

--- server/app.js
+++ server/app.js
@@ -1,42 +1,11 @@
-// const express = require('express'),
-//       http = require('http'),
-//       path = require('path'),
-//       apiRoute = require('./routes/api'),
-//       bodyParser = require('body-parser');
-//       socket = require('socket.io');
-//
-// const app = express(),
-//       port = process.env.PORT || 5000,
-//       clientPath = path.join(__dirname, '../client/src'),
-//       server = http.createServer(app),
-//       io = socket(server);
-//
-// app.use(bodyParser.json());
-// app.use('/api', apiRoute);
-// // app.use(express.static(clientPath));
-// // app.get('*', (req, res) => {
-// //   res.sendFile(clientPath);
-// // });
-//
-// server.listen(port, () => {
-//   console.log(`Server started on port ${port}`);
-// });
-//
-// io.on('connect', socket => {
-//   console.log('user connected');
-// });
-//
-//
-
-
-var app = require('express')();
-var http = require('http');
-var bodyParser = require('body-parser');
-var socket = require('socket.io');
-var redis = require('redis');
-var server = http.createServer(app);
-var io = socket(server);
-var client = redis.createClient();
+const app = require('express')();
+const http = require('http');
+const bodyParser = require('body-parser');
+const socket = require('socket.io');
+const redis = require('redis');
+const server = http.createServer(app);
+const io = socket(server);
+const client = redis.createClient();
 
 const apiRoute = require('./routes/api');
 
@@ -62,7 +31,6 @@ io.on('connection', socket => {
   subscribe.subscribe('pubsub');
 
   subscribe.on('message', (channel, message) => {
-    // console.log(channel, message);
     socket.send(message)
   });
 
